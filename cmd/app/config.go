eb765cc164604d0d22d308dcb9cbd59ac05bb2dc
umeh-promise <umehpromise1000@gmail.com>
2025-01-10 00:02:29 +0100

    feat: implement rate limiting middleware and configuration

--- cmd/app/config.go
+++ cmd/app/config.go
@@ -1,16 +1,24 @@
 package main
 
-import "go.uber.org/zap"
+import (
+	"net/http"
+
+	"github.com/umeh-promise/blog/internal/controller/middlewares"
+	"github.com/umeh-promise/blog/internal/utils"
+	"go.uber.org/zap"
+)
 
 type application struct {
-	config baseConfig
-	logger *zap.SugaredLogger
+	config       baseConfig
+	logger       *zap.SugaredLogger
+	rateLimitter middlewares.RateLimitter
 }
 
 type baseConfig struct {
-	addr string
-	env  string
-	db   dbConfig
+	addr         string
+	env          string
+	db           dbConfig
+	rateLimitter middlewares.RateLimitterConfig
 }
 
 type dbConfig struct {
@@ -18,4 +26,15 @@ type dbConfig struct {
 	maxOpenConn int
 	maxIdleConn int
 	maxIdleTime string
+}
+
+func (app *application) RateLimitMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if allow, retryAfter := app.rateLimitter.Allow(r.RemoteAddr); !allow {
+			utils.RateLimitExceededResponse(w, r, retryAfter.String())
+			return
+		}
+
+		next.ServeHTTP(w, r)
+	})
 }
---
+++ internal/controller/middlewares/ratelimitter_middleware.go
@@ -0,0 +1,56 @@
+package middlewares
+
+import (
+	"sync"
+	"time"
+)
+
+type RateLimitter interface {
+	Allow(ip string) (bool, time.Duration)
+}
+
+type RateLimitterConfig struct {
+	RequestPerTimeFrame int
+	TimeFrame           time.Duration
+}
+
+type FixedWindowRateLimiter struct {
+	sync.RWMutex
+	clients map[string]int
+	limit   int
+	window  time.Duration
+}
+
+func NewFixedWindowLimiter(limit int, window time.Duration) *FixedWindowRateLimiter {
+	return &FixedWindowRateLimiter{
+		clients: make(map[string]int),
+		limit:   limit,
+		window:  window,
+	}
+}
+
+func (rl *FixedWindowRateLimiter) Allow(ip string) (bool, time.Duration) {
+	rl.RLock()
+	count, exists := rl.clients[ip]
+	rl.RUnlock()
+
+	if !exists || count < rl.limit {
+		rl.Lock()
+		if !exists {
+			go rl.resetCount(ip)
+		}
+
+		rl.clients[ip]++
+		rl.Unlock()
+		return true, 0
+	}
+
+	return false, rl.window
+}
+
+func (rl *FixedWindowRateLimiter) resetCount(ip string) {
+	time.Sleep(rl.window)
+	rl.Lock()
+	delete(rl.clients, ip)
+	rl.Unlock()
+}
